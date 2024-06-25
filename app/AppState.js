a741e13d3938aed70432a39804fa57a92f456e71
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-25 10:36:04 -0600

    added cars array to appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,11 +1,18 @@
+import { Car } from './models/Car.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
+  // NOTE leave these properties in the appState for the account and user
   user = null
   /**@type {import('./models/Account.js').Account | null} */
   account = null
+
+  /**
+   * @type {Car[]}
+   */
+  cars = []
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
