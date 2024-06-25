5a7958f88f6b05449ee3852a1998ad7029c305c0
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-25 10:19:29 -0600

    updated note

--- app/services/CarsService.js
+++ app/services/CarsService.js
@@ -3,7 +3,7 @@ import { api } from "./AxiosService.js"
 class CarsService {
   async getCars() {
     // 🐕------> codeWorks sandbox API
-    // NOTE the string passed through as the argument to .get will be concatted to the end of your baseURL
+    // NOTE the string passed through as the argument to .get will be concatted to the end of your baseURL. This will send a GET request to https://sandbox.codeworksacademy.com/api/cars
     const response = await api.get('api/cars')
 
     // NOTE Always log the response data
