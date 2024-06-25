3e4c15004c636cce3b449afb76ef4b9cff422325
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-25 09:26:38 -0600

    init

---
+++ app/env.js
@@ -0,0 +1,7 @@
+// Environment Variables
+
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = ''
+export const audience = ''
+export const clientId = ''
